3c155a8599ad5642dbf5784a3dc0d1379f65534a
sendecka <103881908+sendecka@users.noreply.github.com>
2023-12-26 17:28:45 +0100

    Update 06 Python_Dictionary.md

--- 06 Python_Dictionary.py
+++ 06 Python_Dictionary.py
@@ -1,1 +1,12 @@
+# <p style="text-align: center;">Python Dictionary </p>
 
+### Python Dictionary:
+Słowniki służą do przechowywania danych w parach klucz: wartość. To zbiór uporządkowany, podlegający zmianom i nie pozwalają na duplikaty.
+```
+thisdict = {
+  "brand" : "Ford",
+  "model" : "Mustang",
+  "year" : 1964
+}
+print(thisdict)
+```
